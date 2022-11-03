7fd908211440fc36750fadd69bdfbe2a2093c049
praksh raz <rzprakash16@gmail.com>
2022-11-03 08:20:43 +0545

    made some changes in index.js

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ import chalk from 'chalk';
 
 console.log(chalk.blue('hello good morning'))
 console.log(chalk.bgBlack.underline("aayush"))
+
+
+var a = 10;
+var b = 5;
+
+var c = a+b;
+
+console.log(c)
